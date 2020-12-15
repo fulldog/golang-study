4a3747443dc772ec72c0f7b74ed710521e16f05c
yongkun.xiong <weilone@vip.qq.com>
2020-12-15 18:43:52 +0800

    goday1

--- chitChat/main.go
+++ chitChat/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"chitChat/controller"
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/test", controller.Index)
+	serve := &http.Server{
+		Addr:              ":8888",
+		Handler:           mux,
+		TLSConfig:         nil,
+		ReadTimeout:       0,
+		ReadHeaderTimeout: 0,
+		WriteTimeout:      0,
+		IdleTimeout:       0,
+		MaxHeaderBytes:    0,
+		TLSNextProto:      nil,
+		ConnState:         nil,
+		ErrorLog:          nil,
+		BaseContext:       nil,
+		ConnContext:       nil,
+	}
+	err := serve.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	w.WriteHeader(200)
+	fmt.Println(r.RemoteAddr)
+	_, _ = fmt.Fprint(w, []string{r.RemoteAddr, r.Host, r.Method})
 }
