e5449d6fab7d753ac79779b7d274367d8814149a
“weilone” <767374177@qq.com>
2022-08-10 19:36:03 +0800

    gorm

--- gorm/controller/curd.go
+++ gorm/controller/curd.go
@@ -1,8 +1,13 @@
 package controller
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm/model"
+	"io/ioutil"
+	"strconv"
+	"sync"
 )
 
 func Create(g *gin.Context) {
@@ -15,6 +20,31 @@ func Del(g *gin.Context) {
 }
 
 func Add(g *gin.Context) {
+	by, _ := ioutil.ReadAll(g.Request.Body)
+	if len(by) == 0 {
+		g.JSON(200, gin.H{
+			"message": "没有数据",
+		})
+		return
+	}
+
+	sy := sync.WaitGroup{}
+	sy.Add(1000)
+	sl := make([]*model.Test, 1000)
+	for i := 0; i < 1000; i++ {
+		go func(i int) {
+			t := new(model.Test)
+			json.Unmarshal(by, t)
+			t.Name = t.Name + strconv.Itoa(i)
+			sl[i] = t
+			fmt.Println(t.Add())
+			sy.Done()
+		}(i)
+	}
+	sy.Wait()
+	g.JSON(200, gin.H{
+		"data": sl,
+	})
 
 }
 
--- gorm/model/test.go
+++ gorm/model/test.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Test struct {
 	gorm.Model
-	Name string `gorm:"column:name;not null;size:255;type varchar;comment:用户名"`
-	Desc string `gorm:"column:desc;not null;size:255;type varchar;comment:描述"`
+	Name string `gorm:"column:name;not null;size:255;type varchar;comment:用户名" json:"name"`
+	Desc string `gorm:"column:desc;not null;size:255;type varchar;comment:描述" json:"desc"`
 }
 
 func (receiver Test) TableName() string {
@@ -18,4 +18,8 @@ func (receiver *Test) Create() error {
 		err = Db.Migrator().CreateTable(receiver)
 	}
 	return err
+}
+
+func (receiver *Test) Add() error {
+	return Db.Create(receiver).Error
 }
